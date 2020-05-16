26c7a942a7c77c5d8ff61f530bbcc5cda6535fe3
Shannon Trainor <sltrainor@outlook.com>
2020-05-16 13:23:23 -0400

    added pseudocoding

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+//Doc.Ready to let JS load before running
+
+$(document).ready({
+
+
+    //*************SEARCH BUTTON & SEARCH LOG*****************/
+    //1. Create Event Listener for Search button
+    //2. Check local storage for most recent search history
+            // if no history, set hist variable to empty array []
+            // if history, set hist variable to previous history searches
+    //3. Check if history.length > 0
+            // if yes, call function to search for weather
+    //4. Pass history.length -1 as argument in search weather function
+
+    //**************CURRENT WEATHER*********************/
+    //1. Create ajax call to search weather API for current location
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
